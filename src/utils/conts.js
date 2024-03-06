f3eb987a9e5f673823f8e26fc1d694245fdc72f6
GinoGallina <ginogallina2002@gmail.com>
2024-03-06 15:15:41 -0300

    vercel.json

--- src/utils/conts.js
+++ src/utils/conts.js
@@ -1,11 +1,11 @@
 import { createTheme } from '@mui/material'
 
 export const images = [
-    'public/Images/ameen-almayuf-OQ4tJVXMCYM-unsplash.jpg',
-    'public/Images/kilian-seiler-PZLgTUAhxMM-unsplash.jpg',
-    'public/Images/repair-8321204_1280.jpg',
-    'public/Images/thisisengineering-raeng-IpTPp_aPbYE-unsplash.jpg',
-    'public/Images/work-8304180_1920.jpg',
+    '/images/ameen-almayuf-OQ4tJVXMCYM-unsplash.jpg',
+    '/images/kilian-seiler-PZLgTUAhxMM-unsplash.jpg',
+    '/images/repair-8321204_1280.jpg',
+    '/images/thisisengineering-raeng-IpTPp_aPbYE-unsplash.jpg',
+    '/images/work-8304180_1920.jpg',
 ] // Rutas de las imágenes
 
 export const darkTheme = createTheme({
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Cargar las variables de entorno si es necesario
+import { config as dotenvConfig } from 'dotenv'
+dotenvConfig()
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
 })
